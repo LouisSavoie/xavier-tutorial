add929baf01e093230124d89907a3d29e59e13c7
Louis Savoie <thelousavoie@gmail.com>
2022-08-03 12:36:56 -0400

    rework board rendering

--- eatemup/script.js
+++ eatemup/script.js
@@ -1,43 +1,39 @@
 const boardDisplay = document.getElementById('board-display')
 
-let boardStr = ''
-let newBoardArr = [
-    ['tile','tile','tile','tile','tile','tile','tile','tile','tile'],
-    ['tile','tile','tile','tile','tile','tile','tile','tile','tile'],
-    ['tile','tile','tile','tile','tile','tile','tile','tile','tile'],
-    ['tile','tile','tile','tile','tile','tile','tile','tile','tile'],
-    ['tile','blue','tile','tile','tile','tile','tile','red','tile'],
-    ['tile','tile','tile','tile','tile','tile','tile','tile','tile'],
-    ['tile','tile','tile','tile','tile','tile','tile','tile','tile'],
-    ['tile','tile','tile','tile','tile','tile','tile','tile','tile'],
-    ['tile','tile','tile','tile','tile','tile','tile','tile','tile']
-]
-let boardArr = []
-const boardSize = 10
+let board = ''
+let blue = { y: 4, x: 1 }
+let red = { y: 4, x: 7 }
+const boardSize = 9
 
 // IMPLEMENT: 
+// board reset
 // function to update positions (detect map edges)
 // function to spawn/despawn food, variables to track positions
 // game end detection
 
-function resetBoard() {
-    boardArr = newBoardArr
+function buildBoard() {
+    for (let i = 0; i < boardSize; i++) {
+        for (let j = 0; j < boardSize; j++) {
+            if (i === blue.y && j === blue.x) {
+                board += buildDiv('blue')
+            } else if (i === red.y && j === red.x) {
+                board += buildDiv('red')
+            } else {
+                board += buildDiv('tile')
+            }
+        }
+    }
 }
 
-function buildBoardStr() {
-    boardArr.forEach(i => {
-        i.forEach(j => {
-            boardStr += `<div class="${j}"></div>`
-        })
-    });
+function buildDiv(tile) {
+    return `<div class="${tile}"></div>`
 }
 
 function render() {
-    buildBoardStr()
-    boardDisplay.innerHTML = boardStr
-    boardStr = ''
+    buildBoard()
+    boardDisplay.innerHTML = board
+    board = ''
 }
 
 // TESTING
-resetBoard()
 render()
